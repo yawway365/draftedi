d9e09d57aef116f3afdc032338aba8c5bcc3fd6b
yawway365 <jacksonyawbiz@gmail.com>
2026-02-07 22:42:27 -0700

    Trying to fix the database is locked error

--- app/db/conn.py
+++ app/db/conn.py
@@ -5,8 +5,11 @@ def get_db_path() -> str:
     return os.getenv("DB_PATH", "draftedi.db")
 
 def connect() -> sqlite3.Connection:
-    conn = sqlite3.connect(get_db_path())
+    conn = sqlite3.connect(get_db_path(), timeout=30)
     conn.row_factory = sqlite3.Row
     conn.execute("PRAGMA foreign_keys = ON;")
     conn.execute("PRAGMA journal_mode = WAL;")
+    conn.execute("PRAGMA synchronous = NORMAL;")
+    conn.execute("PRAGMA busy_timeout = 30000;")
+
     return conn
--- app/db/ingest.py
+++ app/db/ingest.py
@@ -20,22 +20,23 @@ def create_edi_file(file_dict):
         file_dict.get("source"),
     )
 
-    conn = connect()
-    cursor = conn.cursor()
+    with connect() as conn:
+        cursor = conn.cursor()
 
-    cursor.execute("""
-        INSERT INTO edi_files
-            (partner_id, interchange_id, processed_at, filename, file_hash, raw_bytes,
-             parse_status, parse_error, processing_state, source)
-        VALUES
-            (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    """, fields)
 
-    file_id = cursor.lastrowid
-    file_dict["file_id"] = file_id
+        cursor.execute("""
+            INSERT INTO edi_files
+                (partner_id, interchange_id, processed_at, filename, file_hash, raw_bytes,
+                    parse_status, parse_error, processing_state, source)
+            VALUES
+                (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, fields)
 
-    conn.commit()
-    conn.close()
+        file_id = cursor.lastrowid
+        file_dict["file_id"] = file_id
+
+        conn.commit()
+        conn.close()
 
     return file_dict
 
@@ -62,24 +63,24 @@ def create_edi_interchange(interchange_dict):
         interchange_dict.get("raw_isa"),
     )
 
-    conn = connect()
-    cursor = conn.cursor()
+    with connect() as conn:
+        cursor = conn.cursor()
 
-    cursor.execute("""
-        INSERT INTO edi_interchanges
-            (file_id, partner_id, interchange_id,
-             isa_control_number, isa_sender_qualifier, isa_sender_id, isa_receiver_qualifier, isa_receiver_id,
-             isa_date, isa_time, usage_indicator, version,
-             element_sep, component_sep, segment_term, repetition_sep, raw_isa)
-        VALUES
-            (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    """, fields)
+        cursor.execute("""
+            INSERT INTO edi_interchanges
+                (file_id, partner_id, interchange_id,
+                isa_control_number, isa_sender_qualifier, isa_sender_id, isa_receiver_qualifier, isa_receiver_id,
+                isa_date, isa_time, usage_indicator, version,
+                element_sep, component_sep, segment_term, repetition_sep, raw_isa)
+            VALUES
+                (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, fields)
 
-    edi_interchange_id = cursor.lastrowid
-    interchange_dict["edi_interchange_id"] = edi_interchange_id
+        edi_interchange_id = cursor.lastrowid
+        interchange_dict["edi_interchange_id"] = edi_interchange_id
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
 
     return interchange_dict
 
@@ -94,22 +95,22 @@ def create_functional_group(group_dict):
         group_dict.get("raw_gs_segment"),
     )
 
-    conn = connect()
-    cursor = conn.cursor()
+    with connect() as conn:
+        cursor = conn.cursor()
 
-    cursor.execute("""
-        INSERT INTO edi_functional_groups
-            (edi_interchange_id, functional_id_code, gs_sender_id, gs_receiver_id,
-             group_control_number, x12_release, raw_gs_segment)
-        VALUES
-            (?, ?, ?, ?, ?, ?, ?)
-    """, fields)
+        cursor.execute("""
+            INSERT INTO edi_functional_groups
+                (edi_interchange_id, functional_id_code, gs_sender_id, gs_receiver_id,
+                group_control_number, x12_release, raw_gs_segment)
+            VALUES
+                (?, ?, ?, ?, ?, ?, ?)
+        """, fields)
 
-    group_id = cursor.lastrowid
-    group_dict["group_id"] = group_id
+        group_id = cursor.lastrowid
+        group_dict["group_id"] = group_id
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
 
     return group_dict
 
@@ -126,22 +127,22 @@ def create_transaction(tx_dict):
         tx_dict.get("ack_status"),
     )
 
-    conn = connect()
-    cursor = conn.cursor()
+    with connect() as conn:
+        cursor = conn.cursor()
 
-    cursor.execute("""
-        INSERT INTO edi_transactions
-            (group_id, transaction_set_id, control_number, implementation_version,
-             segment_count_reported, raw_st_segment, raw_se_segment, ack_status)
-        VALUES
-            (?, ?, ?, ?, ?, ?, ?, ?)
-    """, fields)
+        cursor.execute("""
+            INSERT INTO edi_transactions
+                (group_id, transaction_set_id, control_number, implementation_version,
+                segment_count_reported, raw_st_segment, raw_se_segment, ack_status)
+            VALUES
+                (?, ?, ?, ?, ?, ?, ?, ?)
+        """, fields)
 
-    transaction_id = cursor.lastrowid
-    tx_dict["transaction_id"] = transaction_id
+        transaction_id = cursor.lastrowid
+        tx_dict["transaction_id"] = transaction_id
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
 
     return tx_dict
 
@@ -165,36 +166,36 @@ def lookup_trading_partner_and_interchange(isa_sender_id, isa_receiver_id, sende
     gs_receiver_id  = _clean(gs_receiver_id)
 
     try:
-        conn = connect()
-        cursor = conn.cursor()
+        with connect() as conn:
+            cursor = conn.cursor()
 
-        sql = """
-            SELECT
-                i.interchange_id,
-                i.interchange_partner_id AS partner_id
-            FROM interchanges i
-            WHERE i.isa_sender_id = ?
-              AND i.isa_receiver_id = ?
-              AND i.isa_sender_qualifier = ?
-              AND i.isa_receiver_qualifier = ?
-              AND i.gs_sender_id = ?
-              AND i.gs_receiver_id = ?
-            LIMIT 1
-            """
-        
+            sql = """
+                SELECT
+                    i.interchange_id,
+                    i.interchange_partner_id AS partner_id
+                FROM interchanges i
+                WHERE i.isa_sender_id = ?
+                AND i.isa_receiver_id = ?
+                AND i.isa_sender_qualifier = ?
+                AND i.isa_receiver_qualifier = ?
+                AND i.gs_sender_id = ?
+                AND i.gs_receiver_id = ?
+                LIMIT 1
+                """
+            
 
-        cursor.execute(
-            sql,
-            (isa_sender_id, isa_receiver_id, sender_qual, receiver_qual, gs_sender_id, gs_receiver_id),
-        )
+            cursor.execute(
+                sql,
+                (isa_sender_id, isa_receiver_id, sender_qual, receiver_qual, gs_sender_id, gs_receiver_id),
+            )
 
-        row = cursor.fetchone()
-        conn.close()
+            row = cursor.fetchone()
+            conn.close()
 
-        print(row)
+            print(row)
 
-        if not row:
-            return None, None
+            if not row:
+                return None, None
 
         return int(row["partner_id"]), int(row["interchange_id"])
     except Exception as e:
@@ -219,66 +220,66 @@ def insert_segment_with_elements(
       different repetition_index (1..n)
     - composites: split by component_sep => insert element is_composite=1 + components rows
     """
-    conn = connect()
-    cur = conn.cursor()
+    with connect() as conn:
+        cur = conn.cursor()
 
-    # insert segment row
-    cur.execute(
-        """
-        INSERT INTO edi_segments
-            (transaction_id, position, segment_id, loop_path, raw_segment)
-        VALUES
-            (?, ?, ?, ?, ?)
-        """,
-        (transaction_id, position, segment_id, loop_path, raw_segment),
-    )
-    segment_row_id = cur.lastrowid
+        # insert segment row
+        cur.execute(
+            """
+            INSERT INTO edi_segments
+                (transaction_id, position, segment_id, loop_path, raw_segment)
+            VALUES
+                (?, ?, ?, ?, ?)
+            """,
+            (transaction_id, position, segment_id, loop_path, raw_segment),
+        )
+        segment_row_id = cur.lastrowid
 
-    # elements are 1-based position
-    for element_pos, val in enumerate(element_values, start=1):
-        # val can be "" (blank) and is still "present"
-        if val is None:
-            val = ""
+        # elements are 1-based position
+        for element_pos, val in enumerate(element_values, start=1):
+            # val can be "" (blank) and is still "present"
+            if val is None:
+                val = ""
 
-        reps = [val]
-        if repetition_sep and repetition_sep in val:
-            reps = val.split(repetition_sep)
+            reps = [val]
+            if repetition_sep and repetition_sep in val:
+                reps = val.split(repetition_sep)
 
-        for rep_index, rep_val in enumerate(reps, start=1):
-            # composite?
-            if component_sep and component_sep in rep_val:
-                cur.execute(
-                    """
-                    INSERT INTO edi_elements
-                        (segment_row_id, element_pos, is_composite, value_text, present, repetition_index)
-                    VALUES
-                        (?, ?, 1, NULL, 1, ?)
-                    """,
-                    (segment_row_id, element_pos, rep_index),
-                )
-                element_row_id = cur.lastrowid
-
-                components = rep_val.split(component_sep)
-                for component_pos, cval in enumerate(components, start=1):
+            for rep_index, rep_val in enumerate(reps, start=1):
+                # composite?
+                if component_sep and component_sep in rep_val:
                     cur.execute(
                         """
-                        INSERT INTO edi_components
-                            (element_row_id, component_pos, value_text)
+                        INSERT INTO edi_elements
+                            (segment_row_id, element_pos, is_composite, value_text, present, repetition_index)
                         VALUES
-                            (?, ?, ?)
+                            (?, ?, 1, NULL, 1, ?)
                         """,
-                        (element_row_id, component_pos, cval),
+                        (segment_row_id, element_pos, rep_index),
                     )
-            else:
-                cur.execute(
-                    """
-                    INSERT INTO edi_elements
-                        (segment_row_id, element_pos, is_composite, value_text, present, repetition_index)
-                    VALUES
-                        (?, ?, 0, ?, 1, ?)
-                    """,
-                    (segment_row_id, element_pos, rep_val, rep_index),
-                )
+                    element_row_id = cur.lastrowid
+
+                    components = rep_val.split(component_sep)
+                    for component_pos, cval in enumerate(components, start=1):
+                        cur.execute(
+                            """
+                            INSERT INTO edi_components
+                                (element_row_id, component_pos, value_text)
+                            VALUES
+                                (?, ?, ?)
+                            """,
+                            (element_row_id, component_pos, cval),
+                        )
+                else:
+                    cur.execute(
+                        """
+                        INSERT INTO edi_elements
+                            (segment_row_id, element_pos, is_composite, value_text, present, repetition_index)
+                        VALUES
+                            (?, ?, 0, ?, 1, ?)
+                        """,
+                        (segment_row_id, element_pos, rep_val, rep_index),
+                    )
 
     return segment_row_id
 
@@ -286,20 +287,20 @@ def update_transaction_se_fields(transaction_id: int, segment_count_reported, ra
     """
     Your db.edi_data module doesn't have an update_transaction helper, so we do a small direct update.
     """
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute(
-        """
-        UPDATE edi_transactions
-        SET
-            segment_count_reported = ?,
-            raw_se_segment = ?
-        WHERE transaction_id = ?
-        """,
-        (segment_count_reported, raw_se_segment, transaction_id),
-    )
-    conn.commit()
-    conn.close()
+    with connect() as conn:
+        cur = conn.cursor()
+        cur.execute(
+            """
+            UPDATE edi_transactions
+            SET
+                segment_count_reported = ?,
+                raw_se_segment = ?
+            WHERE transaction_id = ?
+            """,
+            (segment_count_reported, raw_se_segment, transaction_id),
+        )
+        conn.commit()
+        conn.close()
 
 def update_edi_file_status(file_id, parse_status=None, parse_error=None, processing_state=None):
     fields = (
@@ -309,17 +310,17 @@ def update_edi_file_status(file_id, parse_status=None, parse_error=None, process
         file_id,
     )
 
-    conn = connect()
-    cursor = conn.cursor()
+    with connect() as conn:
+        cursor = conn.cursor()
 
-    cursor.execute("""
-        UPDATE edi_files
-        SET
-            parse_status = COALESCE(?, parse_status),
-            parse_error = COALESCE(?, parse_error),
-            processing_state = COALESCE(?, processing_state)
-        WHERE file_id = ?
-    """, fields)
+        cursor.execute("""
+            UPDATE edi_files
+            SET
+                parse_status = COALESCE(?, parse_status),
+                parse_error = COALESCE(?, parse_error),
+                processing_state = COALESCE(?, processing_state)
+            WHERE file_id = ?
+        """, fields)
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
